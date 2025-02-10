9120135ca0cbf1c5d753b12354d17286f1adec74
retrorahul205 <167735447+retrorahul205@users.noreply.github.com>
2025-02-10 10:16:24 +0530

    Add files via upload

---
+++ SI.js
@@ -0,0 +1,21 @@
+function updateTime() {
+    const timeElement = document.getElementById('current-time');
+
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+function openLoginPopup() {
+    document.getElementById('loginPopup').style.display = 'flex';
+}
+
+function closeLoginPopup() {
+    document.getElementById('loginPopup').style.display = 'none';
+}
